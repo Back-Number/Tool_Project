4e5d8e5ee61638525656003cbec32e263f5f6b62
贺千禧 <578445333@qq.com>
2022-07-29 13:41:59 +0800

    feat：完成螺旋归位效果

--- src/pages/example1/index.tsx
+++ src/pages/example1/index.tsx
@@ -3,8 +3,9 @@ import { useEffect } from 'react';
 import * as THREE from 'three';
 // import * as dat from 'dat.gui';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import * as SceneUtils from 'three/examples/jsm/utils/SceneUtils';
-import { createRandomNumber } from '../../utils/utils';
+// import * as SceneUtils from 'three/examples/jsm/utils/SceneUtils';
+// import { createRandomNumber } from '../../utils/utils';
+import CubeStruct from './components/CubeStruct';
 
 /**
  *
@@ -14,52 +15,19 @@ import { createRandomNumber } from '../../utils/utils';
  * @color4 '0xB2A4FF'
  */
 
-class CubeStruct {
-  instance: THREE.Object3D;
-  value: number;
-  scene: THREE.Scene;
-  animate: Function;
-
-  constructor(
-    color: number, // 十六进制颜色
-    index: number[],
-    value: number,
-    scene: THREE.Scene,
-    animate: Function,
-  ) {
-    // 生成立方体实例
-    let geometry = new THREE.BoxGeometry(1, 1, 1); // 几何体
-    let material = new THREE.MeshBasicMaterial({
-      // 材质
-      color: color,
-      side: THREE.DoubleSide,
-    });
-    let lineMaterial = new THREE.MeshLambertMaterial({
-      // 线框材质
-      color: 0xb2a4ff,
-      wireframe: true,
-    });
-    let mList = [material, lineMaterial]; // 材质组
-    this.instance = SceneUtils.createMultiMaterialObject(geometry, mList); // 生成网格对象
-    this.instance.position.set(index[0], index[1], index[2]); // 初始化位置
-    this.value = value; //设置其内置数值
-    this.scene = scene; // 添加场景
-    this.animate = animate.bind(this); // 设置动画
-  }
-
-  // 初始化实例，将其加入场景
-  init(): void {
-    this.scene.add(this.instance);
-  }
-}
+/**
+ *
+ */
 
 const Example1 = (props: any) => {
+  let step = 0;
+
   useEffect(() => {
     let webGLRef = document.getElementById('ref');
     if (webGLRef) {
       // 长宽比例
       let proportion = window.innerWidth / window.innerHeight;
-      const colorList = [0xbbe06c, 0x7cb855, 0x469b4c, 0x3c6e57];
+      const colorList = [0xbbe06c, 0xfcff89, 0xeaafaf, 0xbbded6];
 
       // 创建3要素
       const scene = new THREE.Scene();
@@ -69,9 +37,10 @@ const Example1 = (props: any) => {
       // 渲染场景大小，背景颜色，允许阴影
       renderer.setClearColor(new THREE.Color(0xfff9ca));
       renderer.setSize(window.innerWidth * 0.9, window.innerHeight);
+      renderer.shadowMap.enabled = true;
 
       // 设置相机视角
-      camera.position.set(40, 40, 40);
+      camera.position.set(100, 100, 100);
       camera.lookAt(new THREE.Vector3(0, 0, 0));
 
       // 创建坐标轴
@@ -79,31 +48,33 @@ const Example1 = (props: any) => {
       scene.add(axesHelper);
 
       // 创建平面
-      const g1 = new THREE.PlaneGeometry(60, 60);
+      const g1 = new THREE.PlaneGeometry(100, 100);
       const m1 = new THREE.MeshBasicMaterial({
         color: 0xffdeb4,
         side: THREE.DoubleSide,
       });
       const plane = new THREE.Mesh(g1, m1);
       plane.rotation.x = Math.PI / 2;
+      plane.receiveShadow = true;
       scene.add(plane);
+
+      // 添加聚光灯
+      const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+      directionalLight.position.set(0, 100, -100);
+      scene.add(directionalLight);
 
       // 创建立方体组
       let i = 0;
       let cubuList: CubeStruct[] = [];
-      while (i < 10) {
+      while (i < 360) {
+        // 创建立方体实例
         const cube = new CubeStruct(
           colorList[Math.ceil(Math.random() * 4) - 1],
-          [
-            createRandomNumber(-15, 15),
-            createRandomNumber(0, 15),
-            createRandomNumber(-15, 15),
-          ],
-          createRandomNumber(0, 100),
+          // createRandomNumber(-15, 15),
+          i + 1,
           scene,
           function (this: any) {
-            this.instance.rotation.x += 0.05;
-            this.instance.rotation.y += 0.05;
+            this.instance.position.set(...this.realPosition);
           },
         );
         cube.init();
@@ -116,15 +87,17 @@ const Example1 = (props: any) => {
       controls.update();
 
       // 启动渲染并加载节点
-      const render = () => {
-        requestAnimationFrame(render);
+      const render = (step: number) => {
         controls.update();
-        cubuList.forEach((item) => {
-          item.animate();
-        });
+        if (step < 359) {
+          step += 0.2;
+        }
+        cubuList[Math.floor(step)].animate();
+        requestAnimationFrame(render.bind(this, step));
         renderer.render(scene, camera);
       };
-      render();
+
+      render(step);
 
       webGLRef.appendChild(renderer.domElement);
     }
